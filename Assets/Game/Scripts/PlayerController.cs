577243ce5fb6fcb948fa5c5f7c0c9cf2629f3cc3
baguiostrawberryjam <johnuelleangel@gmail.com>
2026-01-31 22:46:00 +0800

    Hierarchy set-up and preparation

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 25f;
+    public float moveSpeed = 5f;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
